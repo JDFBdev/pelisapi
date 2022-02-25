3867069e71075ec8e5e0f06236dac0d5346e54d7
santiset <sset@fi.uba.ar>
2022-02-25 00:31:43 -0300

    peli style

--- pelisapi/src/components/pelicula/pelicula.js
+++ pelisapi/src/components/pelicula/pelicula.js
@@ -1,8 +1,34 @@
 import React, {useState, useEffect} from "react"
 import { useNavigate } from 'react-router-dom'
 import { useParams } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
+import s from "./pelicula.module.css";
+import ParticleBackground from 'react-particle-backgrounds';
 let apiKey = '9606b913162ebfc8b1e68fc22f824e10';
+
+const settings = {
+    canvas: {
+        canvasFillSpace: true,
+        width: 200,
+        height: 1000,
+        useBouncyWalls: false
+    },
+    particle: {
+      particleCount: 35,
+      color: "#fff",
+      minSize: 2,
+      maxSize: 6
+    },
+    velocity: {
+      minSpeed: 0.2,
+      maxSpeed: 0.4
+    },
+    opacity: {
+      minOpacity: 0,
+      maxOpacity: 0.6,
+      opacityTransitionTime: 10000
+    }
+  }
 
 export default function Pelicula(){
     let params = useParams();
@@ -16,16 +42,25 @@ export default function Pelicula(){
     }, []);
   
     return (
-        <div>
-            <button onClick={()=>Navigate("/")}> Go Back </button> 
-            <h1>{pelicula.title}</h1>
-            <div>
-                <p>{pelicula.release_date}</p>
-                <p>{pelicula.vote_average}</p>
-                <p>{pelicula.runtime}</p>
+        <div className={s.container}>
+            <ParticleBackground settings={settings} className={s.background}/>
+            <div className={s.navbar}>
+                <div className={s.btns}>
+                    <button className={s.btn} onClick={()=>Navigate('/')}>Main page</button>
+                </div>
             </div>
-            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}></img>
-            <p>{pelicula.overview}</p>
+            <div className={s.info}>
+                <h1>{pelicula.title}</h1>
+                <div>
+                    <p>{pelicula.release_date}</p>
+                    <p>{pelicula.vote_average}</p>
+                    <p>{pelicula.runtime}</p>
+                </div>
+                <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}></img>
+                <div className={s.descripcion}>
+                    <p>{pelicula.overview}</p>
+                </div>
+            </div>
         </div>
     )
 }
